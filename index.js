c5a2d5666c1af1fc8211e7608c962bd6db17c958
prashantsingh2408 <prashantsingh2408@gmail.com>
2020-10-28 15:24:27 +0530

    update

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//index.js
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>');
+  });
+  
+http.listen(3002, () => {
+  console.log('listening on *:3002');
+});
